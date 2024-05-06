143b8f5e76a7761e5610025add638b063bed6dee
one_tenth <11056010@ntub.edu.tw>
2024-05-06 23:35:23 +0800

    庫存增刪改

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from mysite.models import Member
+from mysite.models import Member,Product,InventoryChange
 from django.contrib import admin
+
 
 class MemberAdmin(admin.ModelAdmin):
     list_display=['username','id','borndate','gender','phoneNum']
 
 admin.site.register(Member,MemberAdmin)
+admin.site.register(Product)
+admin.site.register(InventoryChange)
--- mysite/models.py
+++ mysite/models.py
@@ -9,4 +9,26 @@ class Member(AbstractUser):
 
     def __str__(self):
         return  self.gender
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
+class InventoryChange(models.Model):
+    CHANGE_TYPES = (
+    ('increase', '增加'),
+    ('decrease', '减少'),
+    ('adjustment', '调整'),
+)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.CharField(max_length=20,default='商品尺寸')
+    price = models.CharField(max_length=20,default='0')
+    color = models.CharField(max_length=20,default='商品顏色')
+    quantity = models.IntegerField()
+    change_type = models.CharField(max_length=20, choices=CHANGE_TYPES)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - 尺寸: {self.size}, 颜色: {self.color}, 價格: {self.price}, 數量: {self.quantity}, 變更類型: {self.get_change_type_display()}"
